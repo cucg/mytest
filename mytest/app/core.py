7831ac8423a275b901fd8f02562868031ef90d74
cucg <814972189@qq.com>
2020-04-11 14:23:58 +0800

    mytest

--- mytest/app/core.py
+++ mytest/app/core.py
@@ -11,7 +11,8 @@ def task():
     # 使用annotate对tem字段进行分组并统计其出现的次数
     info = models.UserInfo.objects.values('tem').annotate(number=Count('tem')).values('tem','number').filter(create_time__gt=nowDate)
     # 每天以覆盖的方式写入
-    with open('userinfo.csv','w+',encoding='utf-8') as f:
+    fileName = 'userinfo.csv'
+    with open(fileName,'w+',encoding='utf-8') as f:
         writer = csv.writer(f,delimiter=' ')
         writer.writerow(['体温','人数'])
         for i in info:
@@ -22,5 +23,5 @@ def task():
     recv_email = '814972189@qq.com'
     email = mail.EmailMessage(subject, message,settings.DEFAULT_FROM_EMAIL, [recv_email])
     # 选择附件发送
-    email.attach_file('userinfo.csv')
+    email.attach_file(fileName)
     email.send()
