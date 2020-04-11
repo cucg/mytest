fe188365e4633fde2c97f642b6dfbfe871ab405e
cucg <814972189@qq.com>
2020-04-11 22:37:01 +0800

    mytest

--- mytest/app/core.py
+++ mytest/app/core.py
@@ -1,5 +1,6 @@
+import os
 import datetime
-import csv
+import xlwt
 from app import models
 from django.core import mail
 from django.db.models import Count
@@ -10,18 +11,24 @@ def task():
     nowDate = datetime.datetime.now().strftime('%Y-%m-%d')
     # 使用annotate对tem字段进行分组并统计其出现的次数
     info = models.UserInfo.objects.values('tem').annotate(number=Count('tem')).values('tem','number').filter(create_time__gt=nowDate)
-    # 每天以覆盖的方式写入
-    fileName = 'userinfo.csv'
-    with open(fileName,'w+',encoding='utf-8') as f:
-        writer = csv.writer(f,delimiter=' ')
-        writer.writerow(['体温','人数'])
-        for i in info:
-            writer.writerow([i['tem'],i['number']])
+    fileName = 'userinfo.xlsx'
+    excel = xlwt.Workbook(encoding="utf-8")
+    sheet = excel.add_sheet('Sheet1')
+    sheet.write(0,0,'体温')	# 不带样式的写入
+    sheet.write(0,1,'人数')
+    flag = 1
+    for i in info:
+        sheet.write(flag,0,i['tem'])
+        sheet.write(flag,1,i['number'])
+        flag += 1
+    # 将写入的数据保存至文件
+    FilePath = os.path.join(settings.BASE_DIR,fileName)
+    excel.save(FilePath)
     # 发送邮件
     subject = '体温检测报告'  # 标题部分
     message = ''.join((nowDate ,'的体温报告')) # 主题部分
-    recv_email = '814972189@qq.com'
+    recv_email = '814972189@qq.com'  # 指定邮箱接收
     email = mail.EmailMessage(subject, message,settings.DEFAULT_FROM_EMAIL, [recv_email])
     # 选择附件发送
-    email.attach_file(fileName)
+    email.attach_file(FilePath)
     email.send()
